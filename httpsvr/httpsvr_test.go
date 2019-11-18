992b70e33c014b636a448105282307199a6a8c55
TungDT <tung.daothanhtung@gmail.com>
2019-11-18 16:23:24 +0700

    fix httpsvr test

--- httpsvr/httpsvr_test.go
+++ httpsvr/httpsvr_test.go
@@ -9,9 +9,14 @@ import (
 )
 
 func TestHttpSvr(t *testing.T) {
+	s := NewServer()
+	s.AddHandler("GET", "/", ExampleHandler())
+	s.AddHandler("GET", "/error", ExampleHandlerError())
+	handler := httpLogger{s.router}
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	ExampleHandler()(w, r)
+	handler.ServeHTTP(w, r)
 	resBody, _ := ioutil.ReadAll(w.Result().Body)
 	var obj struct {
 		Error string
@@ -23,8 +28,8 @@ func TestHttpSvr(t *testing.T) {
 	}
 
 	w1 := httptest.NewRecorder()
-	r1 := httptest.NewRequest("GET", "/", nil)
-	ExampleHandlerError()(w1, r1)
+	r1 := httptest.NewRequest("GET", "/error", nil)
+	handler.ServeHTTP(w1, r1)
 	if w1.Result().StatusCode != http.StatusInternalServerError {
 		t.Error(w1.Result().Status)
 	}
