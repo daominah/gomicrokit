755c8865d66cc3be339354097329cd79be4b7a78
TungDT <tung.daothanhtung@gmail.com>
2020-02-04 06:00:28 +0700

    add UUIDWithHyphen

--- maths/uuid.go
+++ maths/uuid.go
@@ -3,6 +3,7 @@ package maths
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
 )
 
 // GenUUID generates unique 128-bit ID, represent by 32-char hex encoding.
@@ -13,4 +14,20 @@ func GenUUID() string {
 	_, _ = rand.Read(b)
 	r := hex.EncodeToString(b)
 	return r
+}
+
+// This implement does not follow rfc4122 standard,
+// but this follows 8-4-4-4-12 layout
+func GenUUIDWithHyphen() string {
+	id := GenUUID()
+	if len(id) != 32 { // just to be safe
+		return ""
+	}
+	timeLow := id[:8]
+	timeMid := id[8:12]
+	timeHiAndVersion := id[12:16]
+	clockSeq := id[16:20]
+	node := id[20:]
+	return fmt.Sprintf("%v-%v-%v-%v-%v",
+		timeLow, timeMid, timeHiAndVersion, clockSeq, node)
 }
--- maths/uuid_test.go
+++ maths/uuid_test.go
@@ -21,3 +21,21 @@ func TestGenUUID2(t *testing.T) {
 		t.Error(len(ids))
 	}
 }
+
+func TestGenUUID3(t *testing.T) {
+	id := GenUUIDWithHyphen()
+	if len(id) != 36 {
+		t.Error()
+	}
+}
+
+func TestGenUUID4(t *testing.T) {
+	ids := make(map[string]bool)
+	n := 100000
+	for i := 0; i < n; i++ {
+		ids[GenUUIDWithHyphen()] = true
+	}
+	if len(ids) != n {
+		t.Error(len(ids))
+	}
+}
