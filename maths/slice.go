be53d5c6b8f12bdabb56f7d0afa250acfffc4225
TungDT <tung.daothanhtung@gmail.com>
2020-02-10 14:35:11 +0700

    add IndexStrings

--- maths/slice.go
+++ maths/slice.go
@@ -10,3 +10,14 @@ func IndexInts(slice []int, sub int) int {
 	}
 	return -1
 }
+
+// IndexStrings returns the index of the first instance of sub in slice,
+// or -1 if sub is not present in slice
+func IndexStrings(slice []string, sub string) int {
+	for i, v := range slice {
+		if v == sub {
+			return i
+		}
+	}
+	return -1
+}
--- maths/slice_test.go
+++ maths/slice_test.go
@@ -5,10 +5,19 @@ import (
 )
 
 func TestIndexInts(t *testing.T) {
-	if IndexInts([]int{1,6,8,9,3,7}, 8) != 2 {
+	if IndexInts([]int{1, 6, 8, 9, 3, 7}, 8) != 2 {
 		t.Error()
 	}
-	if IndexInts([]int{1,6,8,9,3,7}, 10) != -1 {
+	if IndexInts([]int{1, 6, 8, 9, 3, 7}, 10) != -1 {
+		t.Error()
+	}
+}
+
+func TestIndexStrings(t *testing.T) {
+	if IndexStrings([]string{"1", "6", "8", "9", "3", "7"}, "8") != 2 {
+		t.Error()
+	}
+	if IndexStrings([]string{"1", "6", "8", "9", "3", "7"}, "10") != -1 {
 		t.Error()
 	}
 }
