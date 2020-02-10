7a02b25d9124e5889e9653aaaccb65484e207a02
TungDT <tung.daothanhtung@gmail.com>
2020-02-10 14:42:22 +0700

    fix TestCron

--- gofast/cron_test.go
+++ gofast/cron_test.go
@@ -1,6 +1,7 @@
 package gofast
 
 import (
+	"sync"
 	"testing"
 	"time"
 
@@ -8,13 +9,17 @@ import (
 )
 
 func TestCron(t *testing.T) {
+	var lock sync.Mutex
 	counter := make([]time.Time, 0)
 	job := func() {
+		lock.Lock()
 		counter = append(counter, time.Now())
+		lock.Unlock()
 	}
 	c := NewCron(job, 15*time.Millisecond, 1*time.Millisecond)
 	time.Sleep(300 * time.Millisecond)
 	c.Stop()
+	time.Sleep(50 * time.Millisecond)
 	_ = spew.Dump
 	//spew.Dump(counter)
 	if len(counter) != 20 {
